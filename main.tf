4bd4f7b05307a87a1947c310414b3d1e3bdd48b7
Wong Songhan <songhan89@gmail.com>
2023-03-04 21:37:50 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-learn"
+  location = "southeastasia"
+}
+
+## create a blob storage account with name terraform-learn and resource group terraform-learn
+resource "azurerm_storage_account" "sa" {
+  name                     = "terraformlearnstore"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = "southeastasia"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
